835b8a070821d407d74936a720430688dfd9a93c
fran6t <trautmann@wse.fr>
2021-10-02 14:26:06 +0200

    Centralize sphere javascript

---
+++ PanoDrone/inc-javascript.php
@@ -0,0 +1,22 @@
+<script type="text/javascript" src="dist/fromcdn/three.min.js"></script>
+<script type="text/javascript" src="dist/fromcdn/polyfill.min.js"></script>
+<script type="text/javascript" src="dist/fromcdn/browser.js"></script>
+
+<script type="text/javascript" src="dist/fromcdn/photo-sphere-viewer.js"></script>
+<script type="text/javascript" src="dist/plugins/gyroscope.js"></script>
+
+<script type="text/javascript" src="dist/fromcdn/equirectangular-tiles.js"></script>
+<script type="text/javascript" src="dist/plugins/markers.js"></script>
+<?php
+// Ci dessous premier version mais equirectanglar-tiles.jar fait une erreur dsuperclass
+/*
+<script type="text/javascript" src="node_modules/promise-polyfill/dist/polyfill.js"></script>
+<script type="text/javascript" src="node_modules/uevent/browser.js"></script>
+<script type="text/javascript" src="node_modules/nosleep.js/dist/NoSleep.js"></script>
+<script type="text/javascript" src="dist/photo-sphere-viewer.js"></script>
+<script type="text/javascript" src="dist/plugins/gyroscope.js"></script>
+<script type="text/javascript" src="dist/plugins/stereo.js"></script>
+<script type="text/javascript" src="dist/plugins/markers.js"></script>
+<script type="text/javascript" src="dist/fromcdn/equirectangular-tiles.js"></script>
+*/
+?>
